4ea11987bbd0d78602ae39459c4ac0a53c10a411
kevin <kevinhamel1989@gmail.com>
2015-02-06 18:39:20 +0100

    fenuzoncuez

--- zoro.js
+++ zoro.js
@@ -1,3 +1,12 @@
 /**
  * Created by kevin on 06/02/2015.
  */
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    console.log("hello ma poule");
+    res.end('<p>Voici un paragraphe <strong>HTML</strong> !</p>');
+});
+
+server.listen(8080);
